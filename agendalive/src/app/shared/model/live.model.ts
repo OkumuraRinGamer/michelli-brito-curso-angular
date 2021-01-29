06658801e0184989723b0a0958b17c682a7e7e6f
Pedro Rodrigues <pedroras004@gmail.com>
2021-01-29 09:31:54 -0300

    LAZY+LOADING

---
+++ agendalive/src/app/shared/model/live.model.ts
@@ -0,0 +1,12 @@
+import { SafeResourceUrl } from '@angular/platform-browser';
+
+export class live {
+  id: string;
+  liveName: string;
+  channelName: string;
+  liveDate: string;
+  liveTime: string;
+  liveLink: string;
+  registrationDate: string;
+  urlSafe: SafeResourceUrl;
+}
---
+++ agendalive/src/app/views/lives/lives.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LivesRoutingModule } from './lives-routing.module';
+import { HomeComponent } from './home/home.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { LiveListComponent } from './home/live-list/live-list.component';
+import { LiveFormDialogComponent } from './home/live-form-dialog/live-form-dialog.component';
+import { LocalDateTimePipe } from 'src/app/shared/pipe/local-date-time.pipe';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    LiveListComponent,
+    LiveFormDialogComponent,
+    LocalDateTimePipe,
+  ],
+  imports: [
+    CommonModule,
+    LivesRoutingModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatProgressBarModule,
+  ],
+  providers: [LocalDateTimePipe],
+})
+export class LivesModule {}
